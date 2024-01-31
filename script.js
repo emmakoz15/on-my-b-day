3912b50557ff129678f60e97e28dac18619b6c12
emmakoz15 <emmakozlovsky@gmail.com>
2024-01-31 00:57:01 -0500

    btn 1 working

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-let myBirthday = 
-
-function switchPageToMain(myBirthday,correct){
- if myBirthday === correct
+function change(){
+    let text = document.getElementById("bn").innerText = 'On March 15, 1917, a pivotal moment in Russian history unfolded as the last emperor of Russia, Nicholas II, formally abdicated the throne. This momentous decision marked the culmination of mounting social and political pressures that had been building throughout the Russian Empire. The abdication effectively brought an end to the centuries-old Romanov dynasty, plunging Russia into a period of profound political upheaval and ultimately paving the way for the establishment of the Russian Provisional Government. The events of that day set the stage for the later October Revolution, leading to the rise of the Bolsheviks and the eventual establishment of the Soviet Union'
+    let background = document.getElementById("bn").classList = 'btn1'
+    let  img= document.getElementById("bn").src = '#bad/broken/fix' //fix image   
 }
 
-$("#YourDropDownId").change(function () {
-    alert($("#YourDropDownId").val());
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* let myBirthday = document.getElementById('myBirthday').value
+
+function switchPageToMain(myBirthday){
+    if( myBirthday === '3/15/07'){
+        window.location.href= 'main.html'
+        }
+        else{
+
+        }
+}
+ 
+ */
+
