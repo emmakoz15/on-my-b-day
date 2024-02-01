a6b9ac0553e7e312702edcaaedc6db6d2dce71ab
emmakoz15 <emmakozlovsky@gmail.com>
2024-02-01 10:43:47 -0500

    added an img to btn 1

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
 function change(){
-    let text = document.getElementById("bn").innerText = 'On March 15, 1917, a pivotal moment in Russian history unfolded as the last emperor of Russia, Nicholas II, formally abdicated the throne. This momentous decision marked the culmination of mounting social and political pressures that had been building throughout the Russian Empire. The abdication effectively brought an end to the centuries-old Romanov dynasty, plunging Russia into a period of profound political upheaval and ultimately paving the way for the establishment of the Russian Provisional Government. The events of that day set the stage for the later October Revolution, leading to the rise of the Bolsheviks and the eventual establishment of the Soviet Union'
+    let text = document.getElementById("wawawa").textContent = 'On March 15, 1917, a pivotal moment in Russian history unfolded as the last emperor of Russia, Nicholas II, formally abdicated the throne. This momentous decision marked the culmination of mounting social and political pressures that had been building throughout the Russian Empire. The abdication effectively brought an end to the centuries-old Romanov dynasty, plunging Russia into a period of profound political upheaval and ultimately paving the way for the establishment of the Russian Provisional Government. The events of that day set the stage for the later October Revolution, leading to the rise of the Bolsheviks and the eventual establishment of the Soviet Union'
     let background = document.getElementById("bn").classList = 'btn1'
-    let  img= document.getElementById("bn").src = '#bad/broken/fix' //fix image   
+    let img = document.getElementById("change1").src = "images/ce.jpg"
 }
 
 function nextChangeA(){
     let text = document.getElementById("bns").innerText = "On March 15, 1985, a significant milestone in the history of the internet was achieved as the world's first internet domain name was registered. Symbolics.com, a computer manufacturer based in Cambridge, Massachusetts, secured this historic domain, marking the inception of the Domain Name System (DNS). This event laid the groundwork for the structured and organized addressing of websites on the emerging global network, ultimately shaping the way people navigate and access information on the internet. The registration of Symbolics.com stands as a pivotal moment in the evolution of the digital era, symbolizing the beginning of a new era in communication and connectivity. "
     let background = document.getElementById("bns").classList = 'btn2'
-    let  img= document.getElementById("bns").src = '#bad/broken/fix' //fix image   
+    let  img= document.getElementById("bnsimg").src = 'images/ce.jpg' //fix image   
 }
 
 function nextChangeB(){
     let text = document.getElementById("bnt").innerText = "On the infamous Ides of March, March 15, 44 BCE, the Roman statesman and military general Julius Caesar met a tragic and dramatic end. In a conspiracy orchestrated by a group of senators, including notable figures like Brutus and Cassius, Caesar was assassinated in the Theatre of Pompey. The conspirators, motivated by concerns over the increasing power and perceived tyranny of Caesar, surrounded him under the pretext of presenting a petition. However, as the petition was handed to Caesar, it became a signal for the fatal attack. The assassination marked a turning point in Roman history, leading to political upheavals and eventually the rise of the Roman Empire. The event has since become immortalized in literature, including William Shakespeare's renowned play 'Julius Caesar.' "
     let background = document.getElementById("bnt").classList = 'btn3'
-    let  img= document.getElementById("bnt").src = '#bad/broken/fix' //fix image   
+    let  img= document.getElementById("bntimg").src = '#bad/broken/fix' //fix image   
 }
 
 function nextChangeC(){
     let text = document.getElementById("bnf").innerText = "On March 15, 1972, cinematic history was made as 'The Godfather,' directed by Francis Ford Coppola, premiered. This iconic film, based on Mario Puzo's novel of the same name, unfolded the gripping narrative of the powerful Italian-American Mafia family, the Corleones. The premiere marked a significant moment in the world of cinema, showcasing Marlon Brando's unforgettable portrayal of Vito Corleone and earning him an Academy Award for Best Actor. The Godfather's premiere was a cultural phenomenon, setting the stage for its enduring legacy as one of the greatest films in the crime genre, celebrated for its compelling storytelling, memorable characters, and unparalleled influence on filmmaking. "
     let background = document.getElementById("bnf").classList = 'btn4'
-    let  img= document.getElementById("bnf").src = '#bad/broken/fix' //fix image   
+    let  img= document.getElementById("bnfimg").src = '#bad/broken/fix' //fix image   
 }
 
 function nextChangeD(){
     let text = document.getElementById("bnfi").innerText = "On March 15, 1972, NASA reached a significant milestone in the development of the Space Shuttle program by selecting a three-part configuration for the spacecraft. This decision marked a crucial step in the ambitious endeavor to create a reusable space transportation system. The chosen design consisted of an orbiter, solid rocket boosters, and an external fuel tank. The Space Shuttle, with its unique capability for multiple flights, played a pivotal role in advancing space exploration, enabling missions that ranged from deploying satellites to conducting scientific research in low Earth orbit. The selection of the three-part configuration laid the foundation for the operational Space Shuttle program, which would go on to redefine human spaceflight."
     let background = document.getElementById("bnfi").classList = 'btn5'
-    let  img= document.getElementById("bnfi").src = '#bad/broken/fix' //fix image   
+    let  img= document.getElementById("bnfiimg").src = '#bad/broken/fix' //fix image   
 }
+
+
 
 
 
